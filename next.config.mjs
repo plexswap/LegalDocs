41f25dd8abbbe46998dd00688381feb630d3fdd1
Symplexia Labs <suporte@symplexia.com.br>
2024-10-21 21:59:55 -0300

    Trying Cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   trailingSlash: true, // Replace exportTrailingSlash with trailingSlash
   reactStrictMode: true,
   swcMinify: true,
-  distDir: '.next',
   // Add any other configuration options you need
 }
 
